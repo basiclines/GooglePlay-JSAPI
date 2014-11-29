892dfb75a55e2f335325226552974b3f208f8393
Alex Tatay <altapas@gmail.com>
2014-11-29 17:07:36 +0100

    update rating: localized description and numeric (float) rating

--- scrapers/search.js
+++ scrapers/search.js
@@ -23,7 +23,9 @@ module.exports = {
 			card['url'] = 'https://play.google.com' + card_data.find('.details .title').attr('href');
 			card['company'] = card_data.find('.details .subtitle').attr('title');
 			card['html_description'] = card_data.find('.details .description').text();
-			card['rating'] = card_data.find('.tiny-star').attr('aria-label');
+			card['rating_description'] = card_data.find('.tiny-star').attr('aria-label');
+			var rating_style = card_data.find('.tiny-star .current-rating').attr('style');
+			card['rating'] = parseFloat(rating_style.match(/\d+/g)[0]*5 / 100.0);
 			card['price'] = card_data.find('.price-container .display-price').text();
 
 			items.push(card);
